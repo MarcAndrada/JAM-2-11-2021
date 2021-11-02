6f2fca1750e7eb683c6257470b88828adc5835e7
jtomasenti <jordi.tomas@enti.cat>
2021-11-02 22:26:09 +0100

    Camera y movimiento finalizado

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -13,7 +13,18 @@ public class CharacterBehaviour : MonoBehaviour {
     [SerializeField]
     private float playerSpeed;
 
+    [SerializeField]
+    private Camera mainCamera;
+
     public CharacterController player;
+
+    private Vector3 playerInput;
+
+    private Vector3 movePlayer;
+
+    private Vector3 camForward;
+    private Vector3 camRight;
+
 
     // Start is called before the first frame update
     void Start() {
@@ -26,11 +37,28 @@ public class CharacterBehaviour : MonoBehaviour {
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
 
+        //cogemos el player input y lo escalamos a un vector factor 1
+        playerInput = new Vector3(moveX, 0, moveZ);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
 
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * playerSpeed * Time.deltaTime);
+        
     }
-
-    private void FixedUpdate()
+    void camDirection()
     {
-        player.Move(new Vector3(moveX, 0, moveZ) * playerSpeed * Time.deltaTime);
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
---
+++ Assets/Scripts/PlayerInteractController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractController : MonoBehaviour
+{
+    private bool canInteract;
+    private int companions;
+
+    private InteractuableObjectController objCont;
+
+    private QueueController queueCont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        queueCont = gameObject.GetComponent<QueueController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //si aprieta el boton de interactuar y esta cerca de algun objeto
+        if (true && canInteract && objCont != null)
+        {
+            //comprobamos que el numero de camaradas que tenemos sea suficiente para hacer la accion
+            if (objCont.GetRequiredCompanions() <= companions)
+            {
+                //hacer animacion
+                //si la final pos del objeto es diferente a 0 haremos tp al player a esa posicion
+                if (objCont.GetFinalPos().x != 0 && objCont.GetFinalPos().y != 0)
+                {
+                    //hacemos tp al player a la final pos y desactivamos su mesh 
+                }
+                else
+                {
+                    //remarcar cartel de camaradas necesarios
+                }
+            }
+            //cuando haya acabado la animacion hacemos que el player aparezca de nuevo
+
+        }       
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //detectamos si tenemos algun objeto cerca
+        if (collision.gameObject.tag == "Object"){
+            objCont = collision.GetComponent<InteractuableObjectController>();
+            if (objCont.IsAnim())
+            {    
+                canInteract = true;
+            }
+            else 
+            {
+                companions++;
+                queueCont.AddNewComrade(collision.gameObject);
+                objCont = null;
+                //hacer funcion de añadir camarada a la cola
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision) 
+    {
+        if (collision.gameObject.tag == "Object")
+        {
+            canInteract = false;
+            objCont = null;
+        }
+    }
+
+}
