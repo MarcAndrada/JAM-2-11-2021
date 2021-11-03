cb00766a4902bce829e0b8b2ff25f4053f8d65a5
jtomasenti <jordi.tomas@enti.cat>
2021-11-03 19:30:54 +0100

    Awake para el SceneManager y room

---
+++ Assets/Scripts/AwakeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AwakeController : MonoBehaviour
+{
+    public GameObject SceneManager;
+    private int trueawake = 0;
+    private void Awake()
+    {
+        if (trueawake == 0)
+        {
+            Instantiate(SceneManager);
+            trueawake++;
+        }
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
