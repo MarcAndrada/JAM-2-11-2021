ff28985a7bc111fd5fa0dde4c08202a08b1891e2
jtomasenti <jordi.tomas@enti.cat>
2021-11-04 13:49:15 +0100

    Prefab numero en particulas

---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject particulas;
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = particulas.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+        transform.Rotate(new Vector3(0,0,-1),180);
+        
+    }
+}
