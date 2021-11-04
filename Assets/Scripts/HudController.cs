eab3404857e29ab4dc3b311a5f6a4f15550dd335
Crami <cramitus@gmail.com>
2021-11-04 18:13:39 +0100

    HUD y TUTO

---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject W;
+    [SerializeField]
+    GameObject A;
+    [SerializeField]
+    GameObject S;
+    [SerializeField]
+    GameObject D;
+    [SerializeField]
+    GameObject Mouse;
+    [SerializeField]
+    GameObject Image1;
+    [SerializeField]
+    GameObject Image2;
+
+
+    private float TimeToDestroy = 5;
+    private float TimePassed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimePassed += Time.deltaTime;
+
+        if (TimeToDestroy <= TimePassed)
+        {
+            W.SetActive(false);
+            A.SetActive(false);
+            S.SetActive(false);
+            D.SetActive(false);
+            Image1.SetActive(false);
+            Image2.SetActive(false);
+            Mouse.SetActive(false);
+
+
+        }
+    }
+}
